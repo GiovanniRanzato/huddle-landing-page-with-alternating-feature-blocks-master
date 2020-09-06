e0bedc9a412ecfb789bc34e2db3e9a1b0fc329dd
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2020-09-06 21:18:02 +0200

    mobile layout hero-section

--- src/shared/CallToAction/CallToAction.js
+++ src/shared/CallToAction/CallToAction.js
@@ -2,6 +2,6 @@ import React from 'react'
 import classes from './CallToAction.module.css'
 import {SHARED} from '../../Config/Texts'
 const CallToAction =() =>{
-return <div className={classes.cta} onclick={()=>{console.log("clicked")}}>{SHARED.cta}</div>
+return <div className={classes.cta} onClick={()=>{console.log("clicked")}}>{SHARED.cta}</div>
 }
 export default CallToAction;
